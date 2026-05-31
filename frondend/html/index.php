bb7317c08209c62866a8fa26529d74d03823d484
wyy24 <adcwendy390@gmail.com>
2026-05-31 01:14:33 -0600

    Favicon de Tiburon

--- frondend/html/index.php
+++ frondend/html/index.php
@@ -25,6 +25,7 @@ if ($rol_usuario !== 'admin' && $rol_usuario !== 'administrador') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistema ETS · Panel de Administración</title>
+    <link rel="icon" href="tiburon.png">
     <link href="./../css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
