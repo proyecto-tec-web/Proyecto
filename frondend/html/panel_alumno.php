8c20c726b05447481796a4a0a67f2026047b282c
DiegoSZ675 <diego.asanchezz08@gmail.com>
2026-06-15 10:18:54 -0600

    Agregar enlace de favicon a panel_alumno.php

--- frondend/html/panel_alumno.php
+++ frondend/html/panel_alumno.php
@@ -26,6 +26,7 @@ if ($rol_usuario !== 'alumno') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistema ETS · Panel Alumno</title>
+    <link rel="icon" href="tiburon.png">
     <link href="./../css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <style>
